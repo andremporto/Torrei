a272f016493b0108749241ffd52e980b147aaeb7
André Porto <andreporto@me.com>
2023-07-05 14:51:26 -0300

    Initial Commit

---
+++ Torrei/TorreiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TorreiApp.swift
+//  Torrei
+//
+//  Created by André Porto on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TorreiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
